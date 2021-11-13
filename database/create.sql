42f52b8a10b05a4ac46b7c7727697c38c3b1230a
InesQuarteu <quarteui@gmail.com>
2021-11-13 16:00:19 +0000

    Create create.sql

---
+++ database/create.sql
@@ -0,0 +1,57 @@
+DROP TABLE IF EXISTS WrittenBy;
+DROP TABLE IF EXISTS Author;
+DROP TABLE IF EXISTS IsGenre;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Book;
+DROP TABLE IF EXISTS Language;
+
+
+
+CREATE TABLE Language(
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE Book (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	pagesNumber INT,
+	countOfReview INT,
+	publisher TEXT NOT NULL,
+	publishDay INT,
+	publishMonth INT,
+	publishYear INT,
+	description TEXT NOT NULL,
+	rating FLOAT CHECK (rating >= 0 AND rating <=5),
+	isbn INT NOT NULL,
+	language_id INT REFERENCES Language(id)
+);
+
+CREATE TABLE Review(
+	id	SERIAL PRIMARY KEY,
+	name TEXT NOT NULL,
+	book_id INT REFERENCES Book(id)
+);
+
+CREATE TABLE Genre(
+	id	SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE IsGenre(
+	book_id INT REFERENCES Book(id),
+	genre_id INT REFERENCES Genre(id),
+	PRIMARY KEY (book_id, genre_id)
+);
+
+CREATE TABLE Author (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL
+);
+
+CREATE TABLE WrittenBy(
+	book_id INT REFERENCES Book(id),
+	author_id INT REFERENCES Author(id),
+	PRIMARY KEY (book_id, author_id)
+);
