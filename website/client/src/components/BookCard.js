d777b5e57c5feb0ca36d9b7cb9b425309734cca5
anatcruz <anateresacruz2000@gmail.com>
2021-12-27 22:14:31 +0000

    Add Book cards with placeholders

---
+++ website/client/src/components/BookCard.js
@@ -0,0 +1,18 @@
+import {Card, ListGroup} from "react-bootstrap";
+import {AiFillStar} from "react-icons/ai";
+import { BsFillCalendarWeekFill} from 'react-icons/bs';
+
+export function BookCard() {
+  return (
+    <Card id="bookCard">
+      <Card.Body>
+        <Card.Title><a href="#">The Wicked Lover (Georgian, #2)</a></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><a href="#">by Julia Ross</a></Card.Subtitle>
+        <ListGroup horizontal>
+          <ListGroup.Item><AiFillStar/>3.56</ListGroup.Item>
+          <ListGroup.Item><BsFillCalendarWeekFill/>Published 2005</ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  )
+}
--- website/client/src/pages/MainPage.js
+++ website/client/src/pages/MainPage.js
@@ -1,26 +1,65 @@
 import {NavBar} from "../components/Navbar";
-import {Image} from "react-bootstrap";
-import { BsSearch } from 'react-icons/bs';
+import {Button, Col, Container, Form, Image, Row} from "react-bootstrap";
+import { BsSearch} from 'react-icons/bs';
+import {BookCard} from "../components/BookCard";
 
 export function MainPage() {
   return (
     <>
       <NavBar />
       <div id="mainPage">
-        <form action="#">
+        <Form action="#">
           <div className="searchContainer">
             <Image src="/img/books2.jpg"
                    className="d-inline-block align-top"
                    alt="books">
             </Image>
-            <div className="centered col-md-8">
+            <div className="centered col-md-8 col-sm-9">
                 <div className="buttonIn ">
-                  <input className="col-md-8" type="text" placeholder="Search for books here..." name="search" id="enter"/>
-                    <button id="clear" type="submit"><BsSearch/></button>
+                  <input className="col-md-8 col-sm-9" type="text" placeholder="Search for books here..." name="search" id="enter"/>
+                    <Button id="clear" type="submit"><BsSearch/></Button>
                 </div>
             </div>
           </div>
-        </form>
+          <Container>
+            <Row>
+              <Col className="filtersCol" md={4}>
+                <div>
+                  <h5>Sort by:</h5>
+                  <div className="mb-3">
+                    <Form.Check
+                      type="checkbox"
+                      label={"Publish Date"}
+                    />
+                    <Form.Check
+                      type="checkbox"
+                      label="Publish Date"
+                    />
+                    <Form.Check
+                      type="checkbox"
+                      label="Rating"
+                    />
+                    <Form.Check
+                      type="checkbox"
+                      label="Rating"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <h5>Filter by:</h5>
+                  <Form.Label>Pages Number</Form.Label>
+                  <Form.Range />
+                </div>
+              </Col>
+              <Col md={8} className="resultsCol">
+                <h2>Results</h2>
+                <BookCard/>
+                <BookCard/>
+                <BookCard/>
+              </Col>
+            </Row>
+          </Container>
+        </Form>
       </div>
     </>
   )
