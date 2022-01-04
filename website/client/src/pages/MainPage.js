feccb3398804207e42316565c567d824cd0cc7ac
anatcruz <anateresacruz2000@gmail.com>
2022-01-04 15:59:59 +0000

    Add select and improved mobile design

--- website/client/src/pages/MainPage.js
+++ website/client/src/pages/MainPage.js
@@ -4,8 +4,18 @@ import { BsSearch, BsArrowDown, BsArrowUp} from 'react-icons/bs';
 import {BookCard} from "../components/BookCard";
 import {Footer} from "../components/Footer";
 import {useEffect} from "react";
+import Select from 'react-select';
 
 export function MainPage() {
+  const fields = [
+    {label: "name", value: "name"},
+    {label: "publisher", value: "publisher"},
+    {label: "description", value: "description"},
+    {label: "author", value: "author"},
+    {label: "genres", value: "genres"},
+    {label: "language", value: "language"},
+    {label: "reviews", value: "reviews"}
+  ]
 
   useEffect(() => {
     document.title = "Books4You"
@@ -21,16 +31,16 @@ export function MainPage() {
                    className="d-inline-block align-top"
                    alt="books">
             </Image>
-            <div className="centered col-md-8 col-sm-9">
+            <div className="centered col-9">
                 <div className="buttonIn ">
-                  <input className="col-md-8 col-sm-9" type="text" placeholder="Search for books here..." name="search" id="enter"/>
+                  <input type="text" placeholder="Search for books here..." name="search" id="enter"/>
                     <Button id="clear" type="submit"><BsSearch/></Button>
                 </div>
             </div>
           </div>
           <Container>
             <Row>
-              <Col className="filtersCol" md={4}>
+              <Col className="filtersCol" md={3}>
                 <div>
                   <h5 className="mb-3">Sort by:</h5>
                   <div className="mb-3">
@@ -57,12 +67,33 @@ export function MainPage() {
                   <Form.Label>Pages Number</Form.Label>
                   <Form.Range variant='secondary'/>
                 </div>
+                <div className="select">
+                  <h5 className="mb-3">Search on:</h5>
+                  <Select
+                    isMulti
+                    name="fields"
+                    options={fields}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                  />
+                </div>
               </Col>
-              <Col md={8} className="resultsCol">
+              <Col md={9} className="resultsCol">
                 <h2 className="mb-3">Results</h2>
-                <BookCard/>
-                <BookCard/>
-                <BookCard/>
+                <Row>
+                  <Col md={6}>
+                    <BookCard/>
+                    <BookCard/>
+                    <BookCard/>
+                    <BookCard/>
+                  </Col>
+                  <Col md={6}>
+                    <BookCard/>
+                    <BookCard/>
+                    <BookCard/>
+                    <BookCard/>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Container>
