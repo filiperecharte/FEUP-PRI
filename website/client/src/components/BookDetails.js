864588a4cf0541837385520b861ead6a0bed73bc
anatcruz <anateresacruz2000@gmail.com>
2022-01-05 00:10:52 +0000

    Add ratings and reviews

---
+++ website/client/src/components/BookDetails.js
@@ -0,0 +1,25 @@
+import {Card, ListGroup} from "react-bootstrap";
+
+export function BookDetails({isbn, pages, languages, genres}) {
+  return (
+    <Card id="detailsCard">
+      <Card.Body>
+        <Card.Title>Details</Card.Title>
+        <ListGroup variant="flush">
+          <ListGroup.Item><strong>ISBN: </strong>{isbn}</ListGroup.Item>
+          <ListGroup.Item><strong>Pages Number: </strong>{pages}</ListGroup.Item>
+          <ListGroup.Item><strong>Languages: </strong>{languages.replace(";", ",")}</ListGroup.Item>
+          <ListGroup.Item><strong>Genres: </strong>
+            {
+              genres.map((genre, index) => {
+                return (
+                  index === genres.length-1 ? <a key={index} href="#">{genre}</a> : <a key={index} href="#">{genre}, </a>
+                )
+              })
+            }
+          </ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+    </Card>
+)
+}
