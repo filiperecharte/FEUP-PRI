813fa0d327258b08d79311d5cfa686f8b879d69f
anatcruz <anateresacruz2000@gmail.com>
2022-01-05 21:53:53 +0000

    Changes so Tete can run

--- solr/startup.sh
+++ solr/startup.sh
@@ -5,17 +5,17 @@ precreate-core books
 # Start Solr in background mode so we can use the API to upload the schema
 solr start
 
-sleep 3
+sleep 10
 
 cp /data/synonyms.txt /var/solr/data/books/conf
 
-Schema definition via API
+# Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/books/schema
 
 # Populate collection
-bin/post -c books /data/data.json
+bin/post -c books /data/data_subset.json
 
 # Restart in foreground mode so we can access the interface
 solr restart -f
